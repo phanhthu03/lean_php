9c29b4f191aefd0a042ffdb9b3336398534328e4
phanhthu03 <132880440+phanhthu03@users.noreply.github.com>
2023-07-17 19:07:17 +0700

    action.php

--- category/action.php
+++ category/action.php
@@ -1,11 +1,13 @@
 <?php
     require_once 'pdo.php';
+    $categoryConnection = new CategoryConnection();
+
     $id = ['id' => $_GET['id']];
     $name = $_POST['name'];
     $data = [
         'id' => $id['id'],
         'name' => $name
     ];
-    updateData($data);
+    $categoryConnection->updateData($data);
     header("Location: http://localhost/lean_php/category/index.php");
 ?>
