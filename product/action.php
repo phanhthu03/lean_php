31d5072b975c4dcaa797fb84a37c2296912dc490
phanhthu03 <132880440+phanhthu03@users.noreply.github.com>
2023-07-17 19:25:47 +0700

    action.php

--- product/action.php
+++ product/action.php
@@ -1,11 +1,12 @@
 <?php 
     require_once "pdo.php";
+    $productConnection = new ProductConnection();
     $data = [
         'prodName' => $_POST['name'],
         'prodPrice' => $_POST['price'],
         'cateId' => $_POST['cateId'],
         'id' => $_GET['id']
     ];
-    updateProdData($data);
+    $productConnection->updateProdData($data);
     header("Location: http://localhost/lean_php/product/index.php");
 ?>
